21e8cb1cbb38324802e32734630fe7884bd65f86
HalfDragonLucy <halfdragon14@gmail.com>
2022-01-21 22:25:47 +0100

    Code Optimization, Console Info

--- Program.cs
+++ Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace DDXTextureCompressor
 {
     internal class Program
     {
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr GetConsoleWindow();
-
-        [DllImport("user32.dll")]
-        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private static readonly List<Task> tasks = new List<Task>();
+        private static readonly string[] SizeSuffixes =
+                  { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
         private static void Main(string[] args)
         {
-            IntPtr handle = GetConsoleWindow();
-
-            ShowWindow(handle, 5);
+            Console.WriteLine("Please do not click this window while it's being used. \n");
 
             if (args.Length > 0 && Directory.Exists(args[0]))
             {
@@ -34,34 +32,62 @@ namespace DDXTextureCompressor
                 CreateDDS(file);
             }
 
-            Console.Beep();
+            Console.WriteLine("\nFinished!");
+            Console.WriteLine("Software made by: Half Dragon#3008");
+            Console.WriteLine("Github: https://github.com/HalfDragonLucy/DDXCompressor");
+            Console.ReadKey();
+        }
+
+        private static void DisplayEndInfo(FileInfo oldfile, long newfile)
+        {
+            long oldSize = oldfile.Length;
+            long newSize = newfile;
+            Console.WriteLine($"File optimization status: {oldfile} {SizeSuffix(oldSize)} > {SizeSuffix(newSize)}");
         }
 
         private static void CreateDDS(FileInfo file)
         {
-            File.Move(file.FullName, file.FullName.Replace(Path.GetExtension(file.FullName), ".dds"));
-            string path = file.FullName.Replace(Path.GetExtension(file.FullName), ".dds");
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
 
-            Process proc = new Process();
-            proc.StartInfo.FileName = $"{Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName)}/texconv.exe";
-            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            proc.StartInfo.UseShellExecute = true;
-            proc.EnableRaisingEvents = true;
+                File.Move(file.FullName, Path.ChangeExtension(file.FullName, ".dds"));
+                string f = Path.ChangeExtension(file.FullName, ".dds");
 
-            proc.StartInfo.Arguments = $"{path} -r -pow2 -ft dds -y -o {file.DirectoryName} -f DXT1";
+                Process proc = new Process();
+                proc.StartInfo.FileName = "texconv.exe";
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.EnableRaisingEvents = true;
 
-            proc.Start();
-            proc.WaitForExit();
+                proc.StartInfo.Arguments = $"{f} -r -pow2 -ft dds -y -o {file.DirectoryName} -nologo -f DXT1";
+                proc.Start();
+                while (!proc.HasExited)
+                {
+                }
 
-            File.Move(path, path.Replace(Path.GetExtension(path), ".ddx"));
+                File.Move(f, Path.ChangeExtension(f, ".ddx"));
+                long newFileSize = f.Length;
+                DisplayEndInfo(file, newFileSize);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
+
 
         private static void ProcessDirectory(DirectoryInfo directory)
         {
             foreach (FileInfo file in directory.EnumerateFiles("*.ddx"))
             {
-                CreateDDS(file);
+                tasks.Add(Task.Run(() => { CreateDDS(file); }));
             }
+            Task t = Task.WhenAll(tasks);
+            try
+            {
+                t.Wait();
+            }
+            catch { }
         }
 
         private static void ParseDirectories(string root)
@@ -98,6 +124,21 @@ namespace DDXTextureCompressor
                 DirectoryInfo newDirectory = dest.CreateSubdirectory(subDirectory.Name);
                 CopyDirectoryTree(subDirectory, newDirectory);
             }
+        }
+
+        private static string SizeSuffix(long value)
+        {
+            if (value < 0) { return "-" + SizeSuffix(-value); }
+
+            int i = 0;
+            decimal dValue = value;
+            while (Math.Round(dValue / 1024) >= 1)
+            {
+                dValue /= 1024;
+                i++;
+            }
+
+            return string.Format("{0:n1} {1}", dValue, SizeSuffixes[i]);
         }
     }
 }
